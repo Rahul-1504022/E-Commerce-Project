7702c50f37de3ea479b603543f0a13b7a995dac9
Md.Shahriar Mridha Rahul <54900950+Rahul-1504022@users.noreply.github.com>
2022-10-13 22:47:08 +0600

    Payment Include

--- e-com-backend/server.js
+++ e-com-backend/server.js
@@ -2,6 +2,8 @@ require('dotenv/config');
 const mongoose = require('mongoose');
 const app = require('./app');
 
+global.__basedir = __dirname;
+
 //SERVER connection
 mongoose.connect(process.env.MONGODB_URL_LOCAL)
     .then(() => console.log("Connected to MongoDB!"))
